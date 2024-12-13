1871474d0987101c28b53bada30b995f72b4662d
Анастасия Красько <101454790+Anastasiia2201@users.noreply.github.com>
2024-12-13 03:05:36 +0300

    dnd

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,15 @@
-import React, { useState } from 'react';
-import TodoList from './components/TodoList';
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TodoPage from './pages/TodoPage';
+import DndPage from './pages/DndPage';
 function App() {
-  let data = [
-    { id: 1, title: 'Buy groceries', completed: false, show: true },
-    { id: 2, title: 'Read a book', completed: false,  show: true },
-    ]
- const [todos, setTodos] = useState(data);
- const [newTodo, setNewTodo] = useState('');
- const toggleTodo = (id) => {
-    setTodos(
-    todos.map((todo) => todo.id === id ? { ...todo, completed: !todo.completed } : todo
-    ));
- };
- const addTodo = (e) => {
-    e.preventDefault();
-    if (!newTodo) return;
-
-    const newTodoItem = {
-      id: Date.now(),
-      title: newTodo,
-      completed: false,
-      show: true,
-    };
-    setTodos([...todos, newTodoItem]);
-  };
-
-  const deleteTodo = (id) => {
-    setTodos(todos.filter((item) => item.id != id));
-  };
-
-  const showCompletedTodo = () => {
-    setTodos(
-      todos.map((todo) => todo.completed != true ? { ...todo, show: false} : { ...todo, show: true}
-      ));
-  };
- const showNotCompletedTodo = () => {
-  setTodos(
-    todos.map((todo) => todo.completed != true ? { ...todo, show: true} : { ...todo, show: false}
-    ));
-};
-
-const showAll = () => {
-  setTodos(todos.map(item => {
-    item.show = true;
-    return item;}))
-  };
-
- return (
-  <div class="grid gap-6">
-    <h1 class="text-blue-500" >My To-Do List</h1>
-    <form onSubmit={addTodo}>
-      <input
-      class="border-fuchsia-200 border-8"
-      type="text"
-      value={newTodo}
-      onChange={(e) => setNewTodo(e.target.value)}
-      placeholder="Add new task..."
-      />
-      <button type="submit">Add</button>
-    </form>
-    <TodoList todos={todos} toggleTodo={toggleTodo}  showAll={showAll}
-    deleteTodo={deleteTodo}  showCompletedTodo={showCompletedTodo} showNotCompletedTodo={showNotCompletedTodo}/>
-  </div>
-  );
+return (
+<Router>
+<Routes>
+<Route path="/" element={<TodoPage />} />
+<Route path="/dnd" element={<DndPage />} />
+</Routes>
+</Router>
+);
 }
-
 export default App;
---
+++ src/pages/DndPage.jsx
@@ -0,0 +1,146 @@
+import React, { useState } from 'react';
+import { DragDropContext,
+    Droppable,
+    Draggable } from 'react-beautiful-dnd';
+
+function DndPage() {
+    const [columns, setColumns] = useState({
+        todo: {
+            name: 'To Do',
+            items: [
+                { id: '1', content: 'First task' },
+                { id: '2', content: 'Second task' },
+            ],
+        },
+        inProgress: {
+            name: 'In Progress',
+            items: [],
+        },
+        Done: {
+            name: 'Done',
+            items: [],
+        },
+        Blocked: {
+            name: 'Blocked',
+            items: [],
+        },
+    });
+
+    const deleteTodo = (columnId, index, id) => {
+        console.log(columns[columnId])
+        setColumns({
+            ...columns,
+            [columnId]: {
+                ...columns[columnId],
+                items: columns[columnId].items.filter((item) => item.id != id),
+            },
+        });
+      };
+
+    const onDragEnd = (result, columns, setColumns) => {
+        const { source, destination } = result;
+        if (!destination) return;
+
+        const sourceColumn = columns[source.droppableId];
+        const destColumn = columns[destination.droppableId];
+        const sourceItems = [...sourceColumn.items];
+        const destItems = [...destColumn.items];
+        const [removed] = sourceItems.splice(source.index, 1);
+
+        if (source.droppableId === destination.droppableId) {
+            sourceItems.splice(destination.index, 0, removed);
+            setColumns({
+                ...columns,
+                [source.droppableId]: {
+                    ...sourceColumn,
+                    items: sourceItems,
+                },
+            });
+        } else {
+            destItems.splice(destination.index, 0, removed);
+            setColumns({
+                ...columns,
+                [source.droppableId]: {
+                    ...sourceColumn,
+                    items: sourceItems,
+                },
+                [destination.droppableId]: {
+                    ...destColumn,
+                    items: destItems,
+                },
+            });
+        }
+    };
+
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', height: '100%' }}>
+        <DragDropContext  onDragEnd={(result) => onDragEnd(result, columns, setColumns)}>
+            {Object.entries(columns).map(([columnId, column], index) => {
+                return (
+                    <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        margin: '0 20px',
+                    }}
+                    key={columnId}>
+                    <h2>{column.name}</h2>
+                    <Droppable droppableId={columnId} key={columnId}>
+                        {(provided, snapshot) => {
+                        return (
+                            <div
+                            {...provided.droppableProps}
+                            ref={provided.innerRef}
+                            style={{
+                                background: snapshot.isDraggingOver ? 'lightblue' : 'lightgrey',
+                                padding: 4,
+                                width: 250,
+                                minHeight: 500,
+                            }}>
+                            {column.items.map((item, index) => (
+                                <Draggable
+                                    key={item.id}
+                                    draggableId={item.id}
+                                    index={index}>
+                                    {(provided, snapshot) => {
+                                        return (
+                                            <div
+                                            ref={provided.innerRef}
+                                            {...provided.draggableProps}
+                                            {...provided.dragHandleProps}
+                                            style={{
+                                                display: 'flex',
+                                                flexDirection: 'row',
+                                                alignItems: 'center',
+                                                justifyContent: 'space-between',
+                                                userSelect: 'none',
+                                                padding: 16,
+                                                margin: '0 0 8px 0',
+                                                minHeight: '50px',
+                                                backgroundColor: snapshot.isDragging
+                                                ? '#263B4A'
+                                                : '#456C86',
+                                                color: 'white',
+                                                ...provided.draggableProps.style,}}>
+                                            {item.content}
+                                            <button class="h-3 w-3 bg-red-950 text-white font-bold m-2" onClick={() => deleteTodo(columnId,index, item.id)}>delete</button>
+                                            </div>
+                                        );
+                                    }}
+                                </Draggable>
+                            ))}
+                            {provided.placeholder}
+                            </div>
+                            );
+                        }}
+                    </Droppable>
+                    </div>
+                );
+            })}
+        </DragDropContext>
+        </div>
+    );
+}
+
+export default DndPage;
